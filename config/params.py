dc494960bcd0dcb147c2d428093630f06c9013af
yuhohyun <hohyun07@gmail.com>
2025-06-13 09:49:23 +0000

    update README.md, config/params.py

--- config/params.py
+++ config/params.py
@@ -1,11 +1,11 @@
 PARAMS = {
     "num_channels": 22,
     "time_length": 1000,
-    "fusion_channels": 125,
-    "temporal_channels": 125,
+    "fusion_channels": 150,
+    "temporal_channels": 150,
     "temporal_kernel_sizes": [64, 32, 16],
-    "spatial_kernel_sizes": [3, 5, 7],
-    "spatial_expansion": 32,
-    "mapped_dim": 64,
+    "spatial_kernel_sizes": [3, 7, 11],
+    "spatial_expansion": 45,
+    "mapped_dim": 90,
     "num_classes": 4,
 }
